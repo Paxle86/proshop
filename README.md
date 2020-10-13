00c1fc985a282db162f76da4538e2ad4d399ff9e
Le Thanh <home@Les-iMac.local>
2020-10-13 17:13:20 +0700

    setup backend nodejs

---
+++ README.md
@@ -0,0 +1,1 @@
+Mearn stack e-commercial app 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screen/HomeScreen'
+import ProductScreen from './screen/ProductScreen'
 
 const App = () => {
 	return (
-		<>
-			<h1>Hello world</h1>
-		</>
+		<BrowserRouter>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Route path='/' exact component={HomeScreen} />
+					<Route path='/product/:id' component={ProductScreen} />
+				</Container>
+			</main>
+			<Footer />
+		</BrowserRouter>
 	)
 }
 
